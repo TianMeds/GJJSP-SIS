56e915338202b7118e885211d9d23b022ae07ccf
Christian Medallada <cbmedallada@student.apc.edu.ph>
2023-12-12 15:34:24 +0800

    Commit and changes the store same with school and submission

--- gjjsp-frontend/gjjsp-frontend/src/pages/School/School.jsx
+++ gjjsp-frontend/gjjsp-frontend/src/pages/School/School.jsx
@@ -4,8 +4,10 @@ import Layout from '../Components/Layout';
 import { Search, SearchIconWrapperV2,StyledInputBaseV2 } from '../Components/Styles';
 import useDialogStore from '../Components/store';
 
-export default function School() {
+export default function School({state}) {
   const {
+    anchorEl,
+    setAnchorEl,
     school,
     schoolName,
     schoolAddress,
@@ -15,8 +17,21 @@ export default function School() {
     setSchoolAddress,
     setSchoolType,
     setSchoolPeriod,
+    handleOpenSchool,
+    handleCloseSchool,
     addSchool = ((store) => store.addSchool), 
+    deleteRow = ((store) => store.deleteRow),
   } = useDialogStore();
+
+  const schools = useDialogStore((store) => store.schools.filter((school) => school.state === state));
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+};
+
+  const handleClose = () => {
+    setAnchorEl(null);
+};
   return (
     <Layout>
      <MUI.Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
@@ -27,12 +42,90 @@ export default function School() {
           <MUI.Typography variant="h1" sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>Schools</MUI.Typography>
                     
             {/* Add User Button */}
-            <MUI.Button variant="contained" color="primary" sx={{ textTransform: 'none' }} onClick="">
+            <MUI.Button variant="contained" color="primary" sx={{ textTransform: 'none' }} onClick={handleOpenSchool}>
               Add Schools
             </MUI.Button>
 
           </MUI.Box>
         </MUI.Grid>
+
+        {/* Add User Dialog */}
+        <MUI.Dialog open={school} onClose={handleCloseSchool} fullWidth maxWidth="sm">
+          {/* Content of the Dialog */}
+          <MUI.DialogTitle>Add Schools</MUI.DialogTitle>
+            <MUI.DialogContent>
+              {/* Form Fields of New User*/}
+                <MUI.InputLabel htmlFor="name">Name</MUI.InputLabel>
+                  <MUI.TextField 
+                    placeholder='School Name' 
+                    value={schoolName}
+                    onChange={(e) => setSchoolName(e.target.value)} 
+                    fullWidth
+                    sx={{mb: 2}}
+                  />
+
+                  <MUI.InputLabel htmlFor="schoolAddress">School Address</MUI.InputLabel>
+                  <MUI.TextField 
+                    placeholder='School Address' 
+                    value={schoolAddress}
+                    onChange={(e) => setSchoolAddress(e.target.value)} 
+                    fullWidth 
+                    sx={{mb: 2}}
+                  />
+
+                  <MUI.InputLabel htmlFor="schoolType">Type</MUI.InputLabel>
+                  <MUI.FormControl sx={{  width: '100%', borderRadius: '8px',}}>
+                    <MUI.Select
+                      value={schoolType}
+                      onChange={(e) => setSchoolType(e.target.value)} 
+                      native
+                      sx={{mb: 2}}
+                    >
+                      <option value="" disabled>Select Role</option>
+                      <option value="Administrator">Scholarship Administrator</option>
+                      <option value="Scholar Manager">Scholar Manager</option>
+                      <option value="Scholar">Scholar</option>
+                    </MUI.Select>
+                  
+                  </MUI.FormControl>
+                  
+
+                  <MUI.InputLabel htmlFor="schoolPeriod">Current Period</MUI.InputLabel>
+                  <MUI.FormControl sx={{  width: '100%', borderRadius: '8px',}}>
+                    <MUI.Select
+                      value={schoolPeriod}
+                      onChange={(e) => setSchoolPeriod(e.target.value)} 
+                      native
+                    >
+                      <option value="" disabled>Select Role</option>
+                      <option value="Administrator">Scholarship Administrator</option>
+                      <option value="Scholar Manager">Scholar Manager</option>
+                      <option value="Scholar">Scholar</option>
+                    </MUI.Select>
+                  
+                  </MUI.FormControl>
+
+            </MUI.DialogContent>
+
+              <MUI.DialogActions>
+                {/* Add action buttons, e.g., Save Changes and Cancel */}
+                <MUI.Button onClick={handleCloseSchool} color="primary">
+                  Cancel
+                </MUI.Button>
+                  <MUI.Button onClick={() => { 
+                      addSchool(schoolName, schoolAddress, schoolType, schoolPeriod);
+                      setSchoolName(''),
+                      setSchoolAddress(''),
+                      setSchoolType(''),
+                      setSchoolPeriod(''),
+                      handleCloseSchool();
+                    }}
+                    color="primary">
+                    Add
+                  </MUI.Button>
+              </MUI.DialogActions>
+
+        </MUI.Dialog>
 
 
         <MUI.Container sx={{mt: 4, display: 'flex', alignItems: 'center' }}>
@@ -65,7 +158,6 @@ export default function School() {
               </MUI.FormControl>
         </MUI.Container>
 
-       
        {/* -------- Table Section  ----------*/}
        <MUI.TableContainer sx={{ backgroundColor: '#fbf3f2', margin: '2rem 0 0 1rem' }}>
                   <MUI.Table> 
@@ -79,24 +171,27 @@ export default function School() {
                       </MUI.TableRow>
                       </MUI.TableHead>
                       <MUI.TableBody>
-
-                      <MUI.TableRow  className='user' >
-                      <MUI.TableCell sx={{border: 'none'}}  className='schoolName'></MUI.TableCell>
-                      <MUI.TableCell sx={{border: 'none'}}  className='Address'></MUI.TableCell>
-                      <MUI.TableCell sx={{border: 'none'}}  className='Type'></MUI.TableCell>
-                      <MUI.TableCell sx={{border: 'none'}}  className='CurrentPeriod'></MUI.TableCell>
+                      {schools
+                        .reverse()
+                        .map((school, index) => (
+                          (school.schoolName || school.schoolAddress || school.schoolType || school.schoolPeriod) && (
+                      <MUI.TableRow key={index}  className='user' >
+                      <MUI.TableCell sx={{border: 'none'}}  className='schoolName'>{school.schoolName}</MUI.TableCell>
+                      <MUI.TableCell sx={{border: 'none'}}  className='Address'>{school.schoolAddress}</MUI.TableCell>
+                      <MUI.TableCell sx={{border: 'none'}}  className='Type'>{school.schoolType}</MUI.TableCell>
+                      <MUI.TableCell sx={{border: 'none'}}  className='CurrentPeriod'>{school.schoolPeriod}</MUI.TableCell>
                       <MUI.TableCell sx={{border: 'none'}}>
                           <MUI.IconButton
                           color="inherit"
-                          onClick=""
+                          onClick={handleClick}
                           sx={{ textTransform: 'capitalize' }}
                           >
                           <MUI.MoreHorizIcon />
                           </MUI.IconButton>
                           <MUI.Menu
-                          anchorEl=""
-                          open=""
-                          onClose=""
+                          anchorEl={anchorEl}
+                          open={Boolean(anchorEl)}
+                          onClose={handleClose}
                           anchorOrigin={{
                               vertical: 'bottom',
                               horizontal: 'right',
@@ -110,13 +205,17 @@ export default function School() {
                           <MUI.MenuItem onClick="">
                               Update
                           </MUI.MenuItem>
-                          <MUI.MenuItem >
+                          <MUI.MenuItem onClick={() => {
+                            deleteRow(schools.length - 1);
+                          }} >
                               Delete
                           </MUI.MenuItem>
                           {/* Add more options as needed */}
                           </MUI.Menu>
                       </MUI.TableCell>
                       </MUI.TableRow>
+                      )
+                       ))}
                     </MUI.TableBody>
                   </MUI.Table>
                   <MUI.Divider sx={{width:'100%'}}/>
--- gjjsp-frontend/gjjsp-frontend/src/pages/Submission/Submission.jsx
+++ gjjsp-frontend/gjjsp-frontend/src/pages/Submission/Submission.jsx
@@ -1,24 +1,53 @@
 import React from 'react'
 import * as MUI from '../../import';
 import Layout from '../Components/Layout';
+import { Search, SearchIconWrapperV2,StyledInputBaseV2 } from '../Components/Styles';
+import useDialogStore from '../Components/store';
 
 export default function Submission() {
   return (
     <Layout>
-    <MUI.Grid item xs={12} md={8} lg={9}>
-        <MUI.Paper
-          sx={{
-            p: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            height: 240,
-          }}
-        >
-          <MUI.Typography>
-            Submission
-          </MUI.Typography>
-        </MUI.Paper>
+    <MUI.Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <MUI.Grid container spacing={3}>
+
+      <MUI.Grid item xs={12}>
+        <MUI.Box display="flex" flexDirection={{ xs: 'column', sm: 'row' }} alignItems="center" justifyContent="space-between">
+          
+          <MUI.Typography variant="h1" sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>Submission</MUI.Typography>
+          
+          </MUI.Box>
+        </MUI.Grid> 
+
+        <MUI.Container sx={{mt: 4, display: 'flex', alignItems: 'center' }}>
+          <Search>
+            <SearchIconWrapperV2>
+              <MUI.SearchIcon />
+            </SearchIconWrapperV2>
+            <StyledInputBaseV2
+              placeholder="Search for Scholarship Project or Benefactor"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+                        
+
+        <MUI.IconButton aria-label="filter">
+          <MUI.FilterListIcon />
+        </MUI.IconButton>
+
+        <MUI.FormControl sx={{  width: '180px', border: '2px solid #032539', borderRadius: '8px'}}>
+                  <MUI.Select
+                    native
+                  >
+                    <option value="All">All</option>
+                    <option value="Closed for Application">Closed Application</option>
+                    <option value="Open for Application">Open Application</option>
+                  </MUI.Select>
+               
+              </MUI.FormControl>
+        </MUI.Container>
+
     </MUI.Grid>
+    </MUI.Container>
   </Layout>
   )
 }
