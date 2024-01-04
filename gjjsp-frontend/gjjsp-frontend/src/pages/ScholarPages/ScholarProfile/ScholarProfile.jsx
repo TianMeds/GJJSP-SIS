a5899a3b481879587860db3051efda9dacd4e4ca
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2024-01-04 14:01:18 +0800

    SL-37 Added scholar profile box and created the scholar profile

--- gjjsp-frontend/gjjsp-frontend/src/pages/ScholarPages/ScholarProfile/ScholarProfile.jsx
+++ gjjsp-frontend/gjjsp-frontend/src/pages/ScholarPages/ScholarProfile/ScholarProfile.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from 'react';
 import * as MUI from '../../../import';
 import Layout from '../../../component/Layout/SidebarNavbar/Layout';
+import { ScholarProfileBox } from '../../../component/ProfileBox/ScholarProfileBox';
+import { ProfileHeader } from '../../../component/ProfileHeader/ProfileHeader';
+import theme from '../../../context/theme';
 
 export default function ScholarProfile() {
   return (
     <Layout>
+      <MUI.ThemeProvider theme={theme}>
     <MUI.Grid item xs={12} md={8} lg={9}>
-        <MUI.Paper
-          sx={{
-            p: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            height: 240,
-          }}
-        >
-          <MUI.Typography>
-            Scholar Profile
-          </MUI.Typography>
-        </MUI.Paper>
+
+        <MUI.Box mb={4}>
+            <MUI.Typography variant='h1' sx={{color: 'black', fontWeight: 'bold'}}>Profile</MUI.Typography>
+        </MUI.Box>
+
+        <MUI.Box mb={4}>
+          <MUI.Typography variant='h5'>Manage your personal information, and control which information other people see</MUI.Typography>
+        </MUI.Box>
+
+        <MUI.Box>
+          <MUI.Link>Learn more about our data privacy policy.</MUI.Link>
+        </MUI.Box>
+
+      
+        <ProfileHeader/>
+      
+        <ScholarProfileBox/>
+        
+        
+        
     </MUI.Grid>
+    </MUI.ThemeProvider>
   </Layout>
   )
 }
