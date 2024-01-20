d80b03c31f8a03124e2ba2d3d41936798030773e
Christian Medallada <cbmedallada@student.apc.edu.ph>
2024-01-20 21:17:58 +0800

    Created the File for the Address part of the api

---
+++ gjjsp-backend/gjjsp-backend/app/Http/Controllers/City_MunicipalitiesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City_Municipalities;
+use Illuminate\Http\Request;
+
+class City_MunicipalitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(City_Municipalities $city_Municipalities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, City_Municipalities $city_Municipalities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(City_Municipalities $city_Municipalities)
+    {
+        //
+    }
+}
---
+++ gjjsp-backend/gjjsp-backend/app/Http/Controllers/ZipCodeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ZipCode;
+use Illuminate\Http\Request;
+
+class ZipCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ZipCode $zipCode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ZipCode $zipCode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ZipCode $zipCode)
+    {
+        //
+    }
+}
