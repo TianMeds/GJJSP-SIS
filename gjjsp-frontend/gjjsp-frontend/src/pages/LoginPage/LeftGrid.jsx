8f63d65062456d845b95af214dd9c38988618fd3
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2024-01-30 21:18:35 +0800

    Updated left grid of login page

--- gjjsp-frontend/gjjsp-frontend/src/pages/LoginPage/LeftGrid.jsx
+++ gjjsp-frontend/gjjsp-frontend/src/pages/LoginPage/LeftGrid.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import * as MUI from '../../import'
-import BGIMG from '../../assets/SchoolBG1.jpg';
+import { Box, Grid, Typography } from '@mui/material';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the required styles
+
+import BGIMG1 from '../../../src/assets/BGIMG1.jpg';
+import BGIMG2 from '../../../src/assets/BGIMG2.jpg';
+import BGIMG3 from '../../../src/assets/BGIMG3.jpg';
+import BGIMG4 from '../../../src/assets/BGIMG4.jpg';
+import BGIMG5 from '../../../src/assets/BGIMG5.jpg';
+import BGIMG6 from '../../../src/assets/BGIMG6.jpg';
+import BGIMG7 from '../../../src/assets/BGIMG7.jpg';
+import BGIMG8 from '../../../src/assets/BGIMG8.jpg';
+import BGIMG9 from '../../../src/assets/BGIMG9.jpg';
+import BGIMG10 from '../../../src/assets/BGIMG10.jpg';
 
 
  const LeftGrid = () => {
     {/* When its size to a Tablet mode the Left Grid display none  */}
     const isMobile = MUI.useMediaQuery('(max-width:768px)');
+
+    const images = [BGIMG1, BGIMG2, BGIMG3, BGIMG4, BGIMG5, BGIMG6, BGIMG7, BGIMG8, BGIMG9, BGIMG10];
+
+const carouselSettings = {
+  showArrows: false,
+  showStatus: false,
+  showIndicators: false,
+  infiniteLoop: true,
+  autoPlay: true,
+  interval: 3000,
+};
+
 
   return (
     <>
@@ -24,7 +49,7 @@ import BGIMG from '../../assets/SchoolBG1.jpg';
               flexDirection: 'column', // Stack items vertically
               alignItems: 'center', // Center items horizontally
               justifyContent: 'center', // Center items vertically
-              backgroundImage: `url(${BGIMG})`,
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${BGIMG1})`,
               backgroundRepeat: 'no-repeat',
               backgroundColor: (t) =>
                 t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
