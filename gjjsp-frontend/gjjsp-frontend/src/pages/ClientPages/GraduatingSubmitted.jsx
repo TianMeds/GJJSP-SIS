651cbefc70b0c40f9b67de335d9a715961f5ba00
Christian Medallada <cbmedallada@student.apc.edu.ph>
2024-02-06 01:26:58 +0800

    Added some configuration file to the Manager and Admin Part in the submission

--- gjjsp-frontend/gjjsp-frontend/src/pages/ClientPages/GraduatingSubmitted.jsx
+++ gjjsp-frontend/gjjsp-frontend/src/pages/ClientPages/GraduatingSubmitted.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import * as MUI from '../../import';
+import Layout from '../../component/Layout/SidebarNavbar/Layout';
+import { Link } from 'react-router-dom';
+import theme from '../../context/theme';
+import HistoryIcon from '@mui/icons-material/History';
+import axios from '../../api/axios';
+
+import useLoginStore from '../../store/LoginStore';
+import useAuthStore from '../../store/AuthStore';
+
+//React Hook Form
+import {useForm, Controller } from 'react-hook-form';
+import { DevTool } from "@hookform/devtools";
 
 export default function GraduatingSubmitted() {
   return (
-    <div>GraduatingSubmitted</div>
+    <Layout>
+    <MUI.ThemeProvider theme={theme}>
+        <MUI.Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <MUI.Grid container spacing={3}>
+                <MUI.Grid item xs={12}>
+
+                    <MUI.Typography>Graduating Submitted</MUI.Typography>
+                </MUI.Grid>
+            </MUI.Grid>
+        </MUI.Container>
+    </MUI.ThemeProvider>
+</Layout>
   )
 }
